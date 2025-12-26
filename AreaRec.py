15ff7db83a098d457f096bf87727ba6f7413f153
Priyanshu272003 <priyansh272003@gmail.com>
2025-12-26 06:41:32 +0530

    Area calculator using def

---
+++ AreaRec.py
@@ -0,0 +1,15 @@
+def area(length, width):
+    return length * width
+
+
+length = float(input("Enter length: "))
+
+width = float(input("Enter width: "))
+
+
+ar = area(length, width)
+
+print(ar, "cm^2")
+
+
+
